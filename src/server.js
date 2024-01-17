582796425736f0da6787f34e4fb85118361b5e00
Frezill <153978313+Frezill@users.noreply.github.com>
2024-01-17 16:54:24 +0700

    video31

--- src/server.js
+++ src/server.js
@@ -3,10 +3,18 @@ const express = require('express'); //declare express library
 const path = require('path');//static file
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
+const connection = require('./config/database');
 
 const app = express(); //express application
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
+
+connection.query(
+  'select * from Users u',
+  function (err, results, fields) {
+    console.log(">>>>result: ", results); // results contains rows returned by server
+  }
+);
 
 //config template engine
 configViewEngine(app);
