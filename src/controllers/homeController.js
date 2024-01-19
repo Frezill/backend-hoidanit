4b9beeae4ded18ad385711b19dfde14baad2fc93
Frezill <153978313+Frezill@users.noreply.github.com>
2024-01-19 14:49:25 +0700

    video45

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,5 @@
 const connection = require('../config/database')
-const { getAllUsers } = require('../services/CRUDService')
+const { getAllUsers, getUserById, updateUserById } = require('../services/CRUDService')
 
 const getHomepage = async (req, res) => {
     let results = await getAllUsers();
@@ -31,10 +31,34 @@ const getCreatePage = (req, res) => {
     res.render('create.ejs');
 }
 
+const getUpdatePage = async (req, res) => {
+    const userId = req.params.id;
+
+    let user = await getUserById(userId);
+
+    res.render('edit.ejs', { userEdit: user });
+}
+
+const postUpdateUser = async (req, res) => {
+    let email = req.body.email;
+    let name = req.body.name;
+    let city = req.body.city;
+    let userId = req.body.userId;
+    // let {email, name, city} = req.body;
+    // console.log(">>>> email = ", email, 'name = ', name, 'city = ', city, 'userId: ', userId);
+
+    await updateUserById(email, name, city, userId);
+
+    // res.send('Updated user succeed !');
+    res.redirect('/');
+}
+
 module.exports = {
     getHomepage,
     getABC,
     getHoiDanIt,
     postCreateUser,
     getCreatePage,
+    getUpdatePage,
+    postUpdateUser,
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -2,20 +2,22 @@
 
 const express = require('express');
 const { getHomepage,
-        getABC,
-        getHoiDanIt,
-        postCreateUser,
-        getCreatePage, } = require('../controllers/homeController')
+    getABC,
+    getHoiDanIt,
+    postCreateUser,
+    getCreatePage,
+    getUpdatePage,
+    postUpdateUser, } = require('../controllers/homeController')
 const router = express.Router();
 
 //declare route
 router.get('/', getHomepage);
 router.get('/abc', getABC);
 router.get('/hoidanit', getHoiDanIt);
-
 router.get('/create', getCreatePage);
+router.get('/update/:id', getUpdatePage);
 
 router.post('/create-user', postCreateUser);
-
+router.post('/update-user', postUpdateUser);
 
 module.exports = router;
