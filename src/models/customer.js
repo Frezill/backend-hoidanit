81de54d247db194c748d029cf507130844045928
Frezill <lephatpeter@gmail.com>
2024-02-18 22:29:44 +0700

    test

--- src/models/customer.js
+++ src/models/customer.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     city: String,
+    size: String,
 });
 //create table
 const User = mongoose.model('user', userSchema);
