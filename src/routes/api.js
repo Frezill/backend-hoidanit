35f269b1449dd1140621dfa288ceea2aaa9f4f04
Frezill <lephatpeter@gmail.com>
2024-02-22 08:41:03 +0700

    Video86

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,17 +3,19 @@ const express = require('express');
 const routerAPI = express.Router();
 
 const { getUsersAPI, postCreateUsersAPI, putUpdateUsersAPI, deleteUsersAPI, postUploadSingleFileApi, postUploadMultipleFilesAPI } = require('../controllers/apiController')
+const {postCreateCustomer } = require("../controllers/customerController");
+
 
 //declare route
 
 routerAPI.get('/users', getUsersAPI);
-
 routerAPI.post('/users', postCreateUsersAPI);
-
 routerAPI.put('/users', putUpdateUsersAPI);
-
 routerAPI.delete('/users', deleteUsersAPI);
 
 routerAPI.post('/file', postUploadSingleFileApi);
 routerAPI.post('/files', postUploadMultipleFilesAPI);
+
+routerAPI.post('/customers', postCreateCustomer);
+
 module.exports = routerAPI;
