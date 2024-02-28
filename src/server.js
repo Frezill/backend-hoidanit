29168e384b02959205c3c84f2ae639c28f3be4df
Frezill <lephatpeter@gmail.com>
2024-02-28 20:55:27 +0700

    Video113

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const fileUpload = require('express-fileupload');
 
 const connection = require('./config/database');
 const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 
 const app = express(); //express application
 const port = process.env.PORT || 8888;
@@ -30,7 +31,37 @@ app.use('/v1/api', apiRoutes);
 (async () => {
   //test connection
   try {
-    await connection();
+    //using mongoose
+    // await connection();
+
+    //using mongodb driver
+    // Connection URL
+    const url = process.env.DB_HOST_WITH_DRIVER;
+    const client = new MongoClient(url);
+    // Database Name
+    const dbName = process.env.DB_NAME;
+
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('customers');
+    // let a = await collection.findOne({name: "Hoi Dan IT"});
+    // console.log(">> find: ", a);
+
+    collection.insertOne({
+      "name": "Le Thanh Phat",
+      "phone number": "0395778055",
+      address: {
+        province: "Hau Giang",
+        country: {
+          name: "Viet Nam",
+          code: 65700
+        }
+      }
+    })
+
+
+    //////////////////////////////
     app.listen(port, () => {
       console.log(`Backend app listening on port ${port}`)
     })
